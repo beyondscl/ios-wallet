9135d3b9daf75abd317146eacd1003c80cbf4bb7
CQYH <cqyh@xiongxiangdemac-mini.local>
2018-08-15 10:31:06 +0800

    Initial Commit

---
+++ wwec/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  wwec
+//
+//  Created by CQYH on 2018/8/15.
+//  Copyright © 2018年 CQYH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ wwec/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  wwec
+//
+//  Created by CQYH on 2018/8/15.
+//  Copyright © 2018年 CQYH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}

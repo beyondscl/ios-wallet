c8d62d85504896dce6c96bf4736162ffcd979c67
CQYH <cqyh@xiongxiangdemac-mini.local>
2018-08-15 20:35:02 +0800

    feat:完成icon 启动图，状态栏等界面的东西。

--- wwec/ViewController.m
+++ wwec/ViewController.m
@@ -5,10 +5,12 @@
 //  Created by CQYH on 2018/8/15.
 //  Copyright © 2018年 CQYH. All rights reserved.
 //
+// UIScrollViewDelegate
+// UIWebViewDelegate
 
 #import "ViewController.h"
 
-@interface ViewController ()<UIWebViewDelegate>{
+@interface ViewController ()<UIWebViewDelegate,UIScrollViewDelegate>{
     
 }
 @end
@@ -32,9 +34,35 @@
     UIWebView *webview = [[UIWebView alloc]initWithFrame:self.view.frame];
     webview.backgroundColor = [UIColor blackColor];
     webview.scrollView.scrollEnabled = NO;
+    webview.scalesPageToFit = YES;
     webview.delegate = self;
+    webview.scrollView.delegate = self;
     [webview loadRequest:[NSURLRequest requestWithURL:url]];
     [self.view addSubview:webview];
+}
+
+// uiwebview delegate
+- (BOOL)webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType{
+    return YES;
+}
+- (void)webViewDidStartLoad:(UIWebView *)webView{
+
+}
+- (void)webViewDidFinishLoad:(UIWebView *)webView{
+
+}
+- (void)webView:(UIWebView *)webView didFailLoadWithError:(NSError *)error{
+    
+}
+
+// 唤出键盘内容不移动
+- (UIView*)viewForZoomingInScrollView:(UIScrollView*)scrollView{
+    return nil;
+}
+
+// 背景黑色，状态栏文字白色适配所有机型
+- (UIStatusBarStyle)preferredStatusBarStyle{
+    return UIStatusBarStyleLightContent;
 }
 
 @end
